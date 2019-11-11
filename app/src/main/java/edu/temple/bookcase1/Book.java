60e3a2cf223549d370067898a53d7200b5128381
crystalZhang69164 <tuf69164@temple.edu>
2019-11-10 23:27:23 -0500

    getters and setters for the book

--- app/src/main/java/edu/temple/bookcase1/Book.java
+++ app/src/main/java/edu/temple/bookcase1/Book.java
@@ -10,6 +10,7 @@ public class Book {
     int published;
     String coverURL;
 
+    //constructor for book obj
     public Book(int id, String title, String author, int published, String coverURL){
         this.id = id;
         this.title = title;
@@ -19,5 +20,48 @@ public class Book {
 
     }
 
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public int getPublished() {
+        return published;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
     ArrayList<Book> bookArrayList = new ArrayList<Book>();
+
+
 }
